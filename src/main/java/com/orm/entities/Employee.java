8d506e242b4a434b8f45772cb0dc7113cab71818
Veda9304 <vedasailakshmi001@gmail.com>
2026-05-25 12:04:58 +0530

    One to One done

--- src/main/java/com/orm/entities/Employee.java
+++ src/main/java/com/orm/entities/Employee.java
@@ -7,6 +7,55 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+
+@Entity
 public class Employee {
 	
+	@Id
+	private int emp_id;
+	private String emp_name;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="laptop_id")
+	private Laptop laptop;
+
+	public int getEmp_id() {
+		return emp_id;
+	}
+
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+
+	public String getEmp_name() {
+		return emp_name;
+	}
+
+	public void setEmp_name(String emp_name) {
+		this.emp_name = emp_name;
+	}
+
+	public Laptop getLaptop() {
+		return laptop;
+	}
+
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
+
+	public Employee(int emp_id, String emp_name, Laptop laptop) {
+		super();
+		this.emp_id = emp_id;
+		this.emp_name = emp_name;
+		this.laptop = laptop;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+
 }
+
